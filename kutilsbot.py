e700fcbe74ea317734d7cbdb122ff7520924ac10
brandonjyan <yanjbrandon@gmail.com>
2021-08-04 22:25:49 -0700

    basic commands

--- kutilsbot.py
+++ kutilsbot.py
@@ -5,10 +5,20 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '.kutils ', case_insensitive = True)
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Kutils'))
+    print('Kutils is up and running.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'latency: {round(client.latency * 1000)}ms');
+
+@client.command(aliases=['?'])
+async def info(ctx):
+    await ctx.send("This is the Kutils help page.")
+
 
 client.run(os.getenv('TOKEN'))
