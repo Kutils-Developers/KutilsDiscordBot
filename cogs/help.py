c94bac69f11c6b0a9089b40483f6b18c69583b70
brandonjyan <yanjbrandon@gmail.com>
2021-08-07 01:01:47 -0700

    cog setup

--- cogs/help.py
+++ cogs/help.py
@@ -1,18 +1,38 @@
 import discord
 from discord.ext import commands
 
+# Help Cog
 class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
-
-    @commands.command(aliases=['?'])
-    async def info(self, ctx):
-        await ctx.send("This is the Kutils help page.")
+        help_attributes = {
+            'help': 'Displays help for Kutils commands',
+            'hidden': True,
+            'aliases': ['h', 'info']
+        }
+        help_obj = CustomHelp(command_attrs=help_attributes, verify_checks=False)
+        client.help_command = help_obj
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
             await ctx.send("Invalid command. Type '.kutils help' for more info.")
 
+
+class CustomHelp(commands.HelpCommand):
+
+    async def send_bot_help(self, mapping):
+        return await super().send_bot_help(mapping)
+
+    async def send_cog_help(self, cog):
+        return await super().send_cog_help(cog)
+
+    async def send_group_help(self, group):
+        return await super().send_group_help(group)
+
+    async def send_command_help(self, command):
+        return await super().send_command_help(command)
+
 def setup(client):
     client.add_cog(Help(client))
+    print("Help cog initializing...")
--- cogs/kutils.py
+++ cogs/kutils.py
@@ -1,15 +1,33 @@
 import discord
 from discord.ext import commands
+# from api import kutilsapi
 
+# Kutils Cog
 class Kutils(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send(f'latency: {round(client.latency * 1000)}ms');
         await ctx.send(ctx.guild.id)
+        await ctx.send(f'latency: {round(self.client.latency * 1000)}ms');
 
+    @commands.command()
+    async def add(self, ctx, name, sheet_link, cell_range, time):
+        await ctx.send("add")
+
+    @commands.command()
+    async def remove(self, ctx, name):
+        await ctx.send("remove")
+
+    @commands.command()
+    async def show(self, ctx):
+        await ctx.send("show")
+
+    @commands.command()
+    async def check(self, ctx):
+        await ctx.send("check")
 
 def setup(client):
     client.add_cog(Kutils(client))
+    print("Kutils cog initializing...")
